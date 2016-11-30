16d2d1137c8f5bc10d41475cebb4423136d167e9
VilmarFonseca <vilmarfonsec@gmail.com>
2016-11-30 13:26:12 -0200

    Initial Commit

---
+++ PokePoke/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokePoke
+//
+//  Created by Vilmar Fonseca on 30/11/16.
+//  Copyright © 2016 Vilmar Fonseca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
