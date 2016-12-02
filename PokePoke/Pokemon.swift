81ff2fcf045fefe4df17b83fd9c26b389a85fcf5
VilmarFonseca <vilmarfonsec@gmail.com>
2016-12-02 12:58:00 -0200

    IBOutlets&BackButton

--- PokePoke/Pokemon.swift
+++ PokePoke/Pokemon.swift
@@ -12,6 +12,14 @@ class Pokemon{
     
     fileprivate var _name: String!
     fileprivate var _pokedexId: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _attack: String!
+    private var _nextEvolutionTxt: String!
+    
     
     var name: String{
         
--- PokePoke/PokemonDetailVC.swift
+++ PokePoke/PokemonDetailVC.swift
@@ -13,6 +13,19 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
     
     
     override func viewDidLoad() {
@@ -23,4 +36,8 @@ class PokemonDetailVC: UIViewController {
     }
 
 
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)        
+    }
 }
